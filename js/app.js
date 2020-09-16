9b1eb437983449861d5598c58766c8425e33dcd4
Nutthapon <armnutthapon@hotmail.co.th>
2020-09-16 15:28:52 +0700

    add poster

--- js/app.js
+++ js/app.js
@@ -1,8 +1,3 @@
-document.addEventListener('prechange', function(event) {
-    document.querySelector('ons-toolbar .center')
-        .innerHTML = event.tabItem.getAttribute('label');
-});
-
 document.addEventListener('init', function(event) {
     var page = event.target;
     console.log(page.id);
@@ -10,6 +5,24 @@ document.addEventListener('init', function(event) {
     if (page.id === 'home') {
         // js of tab 1
 
+
+    } else if (page.id === 'search') {
+        // js of tab 2
+
+        document.getElementById("tab2alert").onclick = function() {
+            console.log("Alert Clicked");
+            ons.notification.alert("Alert in Tab2")
+
+        }
+
+    } else if (page.id === 'favorite') {
+        // js of tab 2
+
+        document.getElementById("tab2alert").onclick = function() {
+            console.log("Alert Clicked");
+            ons.notification.alert("Alert in Tab2")
+
+        }
 
     } else if (page.id === 'profile') {
         // js of tab 2
