f176e10e2a31dfcb40d01dfce242f0c69ed2b098
Nutthapon <armnutthapon@hotmail.co.th>
2020-09-16 18:20:43 +0700

    push page

--- js/app.js
+++ js/app.js
@@ -1,39 +1,25 @@
-document.addEventListener('init', function(event) {
+document.addEventListener('init', function (event) {
     var page = event.target;
     console.log(page.id);
 
     if (page.id === 'home') {
-        // js of tab 1
-
+       
 
     } else if (page.id === 'search') {
         // js of tab 2
 
-        document.getElementById("tab2alert").onclick = function() {
-            console.log("Alert Clicked");
-            ons.notification.alert("Alert in Tab2")
 
-        }
 
     } else if (page.id === 'favorite') {
         // js of tab 2
 
-        document.getElementById("tab2alert").onclick = function() {
-            console.log("Alert Clicked");
-            ons.notification.alert("Alert in Tab2")
-
-        }
 
     } else if (page.id === 'profile') {
         // js of tab 2
 
-        document.getElementById("tab2alert").onclick = function() {
-            console.log("Alert Clicked");
-            ons.notification.alert("Alert in Tab2")
-
-        }
 
 
 
     }
 });
+
