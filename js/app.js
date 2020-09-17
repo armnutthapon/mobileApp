91626c619ad9a55c1776b6e018dbb86f47d41ceb
Nutthapon <armnutthapon@hotmail.co.th>
2020-09-17 17:12:56 +0700

    change color

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
-document.addEventListener('init', function(event) {
+document.addEventListener('init', function (event) {
     var page = event.target;
-  console.log(page.id);
+    console.log(page.id);
     if (page.id === 'page1') {
-      page.querySelector('#bw').onclick = function() {
-        document.querySelector('#myNavigator').pushPage('views/detail.html', {data: {title: 'Page 2'}});
-      };
-    } else if (page.id === 'page2') {
-      page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+        page.querySelector('#bw').onclick = function () {
+            document.querySelector('#myNavigator').pushPage('views/detail.html'); page.querySelector('#bb').onclick = function () {
+                document.querySelector('#myNavigator').pushPage('views/detail2.html');
+            };
+        };
+    } else if (page.id === 'page1') {
+        page.querySelector('#bb').onclick = function () {
+            document.querySelector('#myNavigator').pushPage('views/detail2.html');
+        };
     }
-  });
+});
