56c08e528f011097358989ed399dcfb4c4e3cda9
Nutthapon <armnutthapon@hotmail.co.th>
2020-09-22 22:20:36 +0700

    back button

--- js/app.js
+++ js/app.js
@@ -2,15 +2,19 @@ document.addEventListener('init', function (event) {
     var page = event.target;
     console.log(page.id);
     if (page.id === 'page1') {
+        $('#back').hide();
         page.querySelector('#bw').onclick = function () {
-            document.querySelector('#myNavigator').pushPage('views/detail.html'); 
+            document.querySelector('#myNavigator').pushPage('views/blackwidow.html'); 
             page.querySelector('#bb').onclick = function () {
-                document.querySelector('#myNavigator').pushPage('views/detail2.html');
+                document.querySelector('#myNavigator').pushPage('views/bw.html');
             };
         };
-    } else if (page.id === 'page1') {
-        page.querySelector('#bb').onclick = function () {
-            document.querySelector('#myNavigator').pushPage('views/detail2.html');
+    } else if (page.id === 'bw' || page.id === 'bb') {        
+        $('#back').show();      
+        document.querySelector('ons-back-button').onclick = function(event) {
+            document.querySelector('#myNavigator').popPage();
         };
+
     }
+
 });
